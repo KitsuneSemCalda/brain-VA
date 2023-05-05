8d4d4a5eb60efe32330de9fe3479e995dc002a70
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-05-05 10:48:25 -0300

    [Feat]: Declaring class from factory

---
+++ robot/bot.py
@@ -0,0 +1,28 @@
+class WikipediaSearch:
+    def __init__(self, term) -> None:
+        pass
+
+    def search(self):
+        pass
+pass
+
+class Gpt3:
+    def __init__(self, prompt) -> None:
+        pass
+
+    def generate(self):
+        pass
+pass
+
+class PrologFile:
+    def __init__(self, term, content):
+        pass
+
+    def create(self):
+        pass
+pass
+
+class ChatBotFactory:
+    def create_chatbot(self, term):
+        pass
+pass
